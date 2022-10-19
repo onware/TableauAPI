12a0e6af4e71bb277ccdefb0092ad38f3716ed0c
Warren Blanchet <warren.blanchet@onware.com>
2022-10-19 11:06:53 -0600

    Added publication of data source from file

---
+++ TableauAPI/RESTRequests/InitiateFileUpload.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using TableauAPI.FilesLogging;
+using TableauAPI.RESTHelpers;
+using TableauAPI.ServerData;
+
+namespace TableauAPI.RESTRequests
+{
+    /// <summary>
+    /// Initiates the upload process for a file
+    /// </summary>
+    public class InitiateFileUpload : TableauServerSignedInRequestBase
+    {
+        private readonly TableauServerUrls _onlineUrls;
+
+        /// <summary>
+        /// Create the request to begin uploading a file to the Tableau REST API
+        /// </summary>
+        /// <param name="onlineUrls">Tableau Server Information</param>
+        /// <param name="logInInfo">Tableau Sign In Information</param>
+        public InitiateFileUpload(TableauServerUrls onlineUrls, TableauServerSignIn logInInfo)
+            : base(logInInfo)
+        {
+            _onlineUrls = onlineUrls;
+        }
+
+        /// <summary>
+        /// Execute the REST API call to initiate a file upload - returns the file upload session
+        /// </summary>
+        public string ExecuteRequest()
+        {
+            var statusLog = OnlineSession.StatusLog;
+            string fileUploadSession = "";
+
+            string urlInitiate = _onlineUrls.Url_InitiateFileUpload(OnlineSession);
+            statusLog.AddStatus("Initiating File Upload");
+            try
+            {
+                var webRequest = CreateLoggedInWebRequest(urlInitiate, "POST");
+                var response = GetWebResponseLogErrors(webRequest, "initiate file upload");
+                var xmlDoc = GetWebResponseAsXml(response);
+
+                var nsManager = XmlHelper.CreateTableauXmlNamespaceManager("iwsOnline");
+                var result = xmlDoc.SelectSingleNode("//iwsOnline:tsResponse/iwsOnline:fileUpload", nsManager);
+                fileUploadSession = result.Attributes?["uploadSessionId"]?.Value ?? "";
+            }
+            catch (Exception ex)
+            {
+                statusLog.AddError("Error during file upload initiate\r\n  " + urlInitiate + "\r\n  " + ex.ToString());
+            }
+
+            //Return the set of successfully downloaded content
+            return fileUploadSession;
+        }
+    }
+}
---
+++ TableauAPI/RESTRequests/PublishDataSourceFinish.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using TableauAPI.FilesLogging;
+using TableauAPI.RESTHelpers;
+using TableauAPI.ServerData;
+
+namespace TableauAPI.RESTRequests
+{
+    /// <summary>
+    /// Finishes publication of a data source from previously uploaded data
+    /// </summary>
+    public class PublishDataSourceFinish : TableauServerSignedInRequestBase
+    {
+        private readonly TableauServerUrls _onlineUrls;
+        private readonly string _uploadSession;
+        private readonly string _datasourceFileType;
+        private readonly XmlDocument _requestXml;
+
+        /// <summary>
+        /// Create the request to finish publication of a data source from previously uploaded data in the Tableau REST API
+        /// </summary>
+        /// <param name="onlineUrls">Tableau Server Information</param>
+        /// <param name="logInInfo">Tableau Sign In Information</param>
+        /// <param name="uploadSession">The upload session identifier</param>
+        /// <param name="datasourceFileType">The datasource file type</param>
+        /// <param name="requestXml">The request XML for datasource publication</param>
+        public PublishDataSourceFinish(TableauServerUrls onlineUrls, TableauServerSignIn logInInfo, string uploadSession, string datasourceFileType, XmlDocument requestXml)
+            : base(logInInfo)
+        {
+            _onlineUrls = onlineUrls;
+            _uploadSession = uploadSession;
+            _datasourceFileType = datasourceFileType;
+            _requestXml = requestXml;
+        }
+
+        /// <summary>
+        /// Creates the XML used in the request to finish publication of a data source from previously uploaded data in the Tableau REST API
+        /// </summary>
+        /// <param name="datasourceName">The name of the datasource</param>
+        /// <param name="datasourceDescription">The description for the datasource (can be null)</param>
+        /// <param name="projectId">The project id for the datasource (can be null)</param>
+        public static XmlDocument BuildRequestXml(string datasourceName, string datasourceDescription, string projectId)
+        {
+            XmlDocument doc = new XmlDocument();
+            var tsRequest = doc.CreateElement("tsRequest");
+            doc.AppendChild(tsRequest);
+            var datasource = doc.CreateElement("datasource");
+            tsRequest.AppendChild(datasource);
+            datasource.SetAttribute("name", datasourceName);
+            if (!string.IsNullOrEmpty(datasourceDescription))
+            {
+                datasource.SetAttribute("description", datasourceDescription);
+            }
+            if (!string.IsNullOrEmpty(projectId))
+            {
+                var project = doc.CreateElement("project");
+                datasource.AppendChild(project);
+                project.SetAttribute("id", projectId);
+            }
+            return doc;
+        }
+
+        /// <summary>
+        /// Execute the REST API call to publish a file uploaded in seperate requests.
+        /// </summary>
+        public void ExecuteRequest()
+        {
+            var statusLog = OnlineSession.StatusLog;
+
+            string urlPublish = _onlineUrls.Url_FinalizeDataSourcePublish(OnlineSession, _uploadSession, _datasourceFileType);
+            statusLog.AddStatus("Publishing previously uploaded file");
+            try
+            {
+                var webRequest = CreateAndSendMimeLoggedInRequest(urlPublish, "POST", new MimeWriterXml(_requestXml.OuterXml));
+                var response = GetWebResponseLogErrors(webRequest, "file publish uploaded");
+            }
+            catch (Exception ex)
+            {
+                statusLog.AddError("Error during file upload append\r\n  " + urlPublish + "\r\n  " + ex.ToString());
+            }
+        }
+    }
+}
