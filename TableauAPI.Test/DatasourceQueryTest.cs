c44fa4c7f977f077d4373870c7ce8632013c1cde
TiannanXu <remiliaforever@gmail.com>
2022-12-06 11:12:52 -0700

    Add Graphql Metadata query method
    Take serialized query json object
    Structure reference: https://graphql.org/learn/serving-over-http/#post-request

--- TableauAPI.Test/DatasourceQueryTest.cs
+++ TableauAPI.Test/DatasourceQueryTest.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using TableauAPI.RESTHelpers;
 using TableauAPI.RESTRequests;
+using Newtonsoft.Json;
 
 namespace TableauAPI.Test
 {
@@ -10,6 +11,13 @@ namespace TableauAPI.Test
     {
         private TableauServerUrls _urls;
         private TableauServerSignIn _session;
+
+        public class graphqlRequest
+        {
+            public string query { get; set; }
+            //public string operationName { get; set; } optional property
+            //public string variables { get; set; } optional property
+        }
 
         [TestInitialize]
         public void Login()
@@ -31,6 +39,21 @@ namespace TableauAPI.Test
             var pagedResults = pagedRequest.Datasources;
 
             Assert.IsTrue(pagedResults.Count >= unpagedResults.Count, "paged returns more or same (if unpaged)");
+        }
+
+        [TestMethod]
+        public void TestMetadata()
+        {
+            
+            var meta = new QueryGraphqlMetadata(_urls, _session);
+
+            var q = new graphqlRequest()
+            {
+                query = "query example{\r\n    datasources\r\n    {\r\n      id\r\n    }\r\n}"
+            };
+            string query = JsonConvert.SerializeObject(q);
+            meta.Execute(query);
+            var result = meta.Result;
         }
 
         [TestMethod]
