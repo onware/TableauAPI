409c381e4386bf7f9d6e83c0d46aed1762bb0979
Warren Blanchet <warren.blanchet@onware.com>
2023-06-16 15:58:25 -0600

    DEV-12325 Loosened parsing requirements for ExtractRefreshTask

--- TableauAPI/ServerData/ExtractRefreshTask.cs
+++ TableauAPI/ServerData/ExtractRefreshTask.cs
@@ -29,25 +29,25 @@ namespace TableauAPI.ServerData
         public ExtractRefreshTask(XmlNamespaceManager nsManager, XmlNode task, string _siteId) {
 
             var extractRefresh = task.LastChild;
-            var schedule = task.LastChild.FirstChild;
-            var datasource = task.LastChild.LastChild;
+            var schedule = task.LastChild?.FirstChild;
+            var datasource = task.LastChild?.LastChild;
             
-            datasourceId = datasource.Attributes?["id"].Value;
+            datasourceId = datasource?.Attributes?["id"]?.Value;
 
-            extractRefreshId = extractRefresh.Attributes?["id"].Value;
-            extractRefreshPriority = extractRefresh.Attributes?["priority"].Value;
-            consecutiveFailedCount = extractRefresh.Attributes?["consecutiveFailedCount"].Value;
-            extractRefreshType = extractRefresh.Attributes?["type"].Value;
+            extractRefreshId = extractRefresh?.Attributes?["id"]?.Value;
+            extractRefreshPriority = extractRefresh?.Attributes?["priority"]?.Value;
+            consecutiveFailedCount = extractRefresh?.Attributes?["consecutiveFailedCount"]?.Value;
+            extractRefreshType = extractRefresh?.Attributes?["type"]?.Value;
 
-            scheduleId = schedule.Attributes?["id"].Value;
-            scheduleName = schedule.Attributes?["name"].Value;
-            scheduleState = schedule.Attributes?["state"].Value;
-            schedulePriority = schedule.Attributes?["priority"].Value;
-            scheduleCreatedAt = schedule.Attributes?["createdAt"].Value;
-            scheduleUpdatedAt = schedule.Attributes?["updatedAt"].Value;
-            scheduleType = schedule.Attributes?["type"].Value;
-            scheduleFrequency = schedule.Attributes?["frequency"].Value;
-            scheduleNextRunAt = schedule.Attributes?["nextRunAt"].Value;
+            scheduleId = schedule?.Attributes?["id"]?.Value;
+            scheduleName = schedule?.Attributes?["name"]?.Value;
+            scheduleState = schedule?.Attributes?["state"]?.Value;
+            schedulePriority = schedule?.Attributes?["priority"]?.Value;
+            scheduleCreatedAt = schedule?.Attributes?["createdAt"]?.Value;
+            scheduleUpdatedAt = schedule?.Attributes?["updatedAt"]?.Value;
+            scheduleType = schedule?.Attributes?["type"]?.Value;
+            scheduleFrequency = schedule?.Attributes?["frequency"]?.Value;
+            scheduleNextRunAt = schedule?.Attributes?["nextRunAt"]?.Value;
 
             siteId = _siteId;
         }
